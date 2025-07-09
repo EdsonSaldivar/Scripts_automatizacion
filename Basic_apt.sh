70376c7ff269ae760e727dca49287978f2607db8
Edson Saldivar <137822795+EdsonSaldivar@users.noreply.github.com>
2025-07-09 15:16:45 -0600

    Create Basic_apt_desktop_mint.sh

---
+++ Basic_apt.sh
@@ -0,0 +1,7 @@
+sudo apt update -y
+
+sudo apt upgrade -y
+
+#Para servidores elimina el paquete neofetch, flameshot y openvpn 
+
+sudo apt install htop rsync zip git curl wget vim nano neofetch flameshot traceroute dnsutils net-tools iputils-ping openvpn timeshift -y
